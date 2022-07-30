dfb70d1c8c8b828f7d86b722ce0e1cd808679636
wiggmund <40165054+Wiggmund@users.noreply.github.com>
2022-07-30 17:59:26 +0300

    First successful execution

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,34 @@
 module.exports = {
-  "settings": {
-    "react": {
-      "version": "detect"
-    }
-  },
-  "env": {
-    "browser": true,
-    "es2021": true,
-  },
-  "extends": [
-    "plugin:react/recommended",
-    "google",
-  ],
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true,
-    },
-    "ecmaVersion": "latest",
-    "sourceType": "module",
-  },
-  "plugins": [
-    "react",
-    "@typescript-eslint",
-  ],
-  "rules": {
-    "linebreak-style": ["error", "windows"],
-    "react/react-in-jsx-scope": "off",
-    "indent": ["error", "tab"],
-    "no-tabs": "off"
-  },
+	'settings': {
+		'react': {
+			'version': 'detect'
+		}
+	},
+	'env': {
+		'browser': true,
+		'es2021': true
+	},
+	'extends': [
+		'plugin:react/recommended',
+		'google'
+	],
+	'parser': '@typescript-eslint/parser',
+	'parserOptions': {
+		'ecmaFeatures': {
+			'jsx': true
+		},
+		'ecmaVersion': 'latest',
+		'sourceType': 'module'
+	},
+	'plugins': [
+		'react',
+		'@typescript-eslint'
+	],
+	'rules': {
+		'linebreak-style': ['error', 'windows'],
+		'react/react-in-jsx-scope': 'off',
+		'indent': ['error', 'tab'],
+		'no-tabs': 'off',
+		'comma-dangle': ['error', 'never']
+	}
 };
---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom/client';
+
+
+const root = ReactDOM.createRoot(
+	document.getElementById('root') as Element
+);
+
+const element = <h1>Lorem, ipsum dolor.</h1>;
+root.render(element);
